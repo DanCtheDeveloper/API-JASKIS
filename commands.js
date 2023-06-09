e692a94d5c5f49cb2fc5967c5fe52026b63e9a1a
DantheDeveleoper <119366607+DantheDeveleoper@users.noreply.github.com>
2023-06-09 10:10:54 -0700

    Completed Assignment

--- commands.js
+++ commands.js
@@ -3,35 +3,93 @@
 
 // GETTING STARTED
 // 1. Create a database called jaskis
-
+use jaskis
 
 // 2. Create a collection called bounties
-
+db.createCollection('bounties')
 
 // ADD THE ANIMAL BOUNTIES
 // 1. Insert the given "Thanoceros" bounty object
 
+db.bounties.insertOne({
+    name: "Thanoceros",
+    species: "Rhinoceros",
+    location: "Grasslands",
+    wantedFor: "Eating too much grass",
+    client: "Songbird",
+    reward: 10000,
+    captured: false
+    })
 
 // 2. Query for all bounties in the bounties collection
 
+db.bounties.find()
 
 // 3. Insert many bounties at once using the given objects
+db.bounties.insertMany[{
+    //...data....
+    //...data....
+    }]
 
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
-
+db.bounties.find({ location: "Grasslands"})
 // 2. Query for all bounties with a reward worth 10000 or more
-
+db.bounties.find({ reward: {$gt: 10000}})
 // 3. Query for all bounties, but exclude the client attribute from being shown
-
+db.bounties.find({}, {client:0})
 // 4. Query for a Groundhog in the Woodlands
-
+db.bounties.find({
+    species: "Groundhog",
+    location: "Woodlands"
+  })
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
+db.bounties.updateOne(
+    {name:'Polarwind'},
+    {$set: {reward: 10000}}
+  )
 
 // 2. Remove Lokinkajou
-
+db.bounties.deleteOne(
+    {name: 'Lokinkajou'}
+)
 // 3. Delete all bounties sent by Songbird
-
+db.bounties.deleteMany(
+    {client: 'Songbird'}
+)
 // 4. Update all captured statuses to true
+db.bounties.updateMany(
+    {},    
+  {$set:{captured: true}}
+  )
+
+  //Bonus Activities:
+  //Make a new collection in the JASKIS database called scavengers and insert all the given Scavenger data:
+
+  db.scavengers.insertMany([
+    //new data from replit...
+    //new data from replit...
+    //new data from replit...
+    //new data from replit...
+  ])
+
+//   Find all Scavengers who joined the team after December 31, 2011:
+db.scavengers.find({joined:{$gt: new Date('2011-12-31')}})
+
+//Find all Scavengers who helped catch Thanoceros:
+db.scavengers.find({
+    "captured.name": "Thanoceros"
+})
+
+//Find all Scavengers who helped catch an animal bounty with a reward greater than $11000:
+
+db.scavengers.find({
+    "captured.reward": {$gt: 11000}
+})
+
+//Find all Scavengers that don't have a weapon.
+db.scavengers.find({ weapon: { $exists: false } })
+//or
+db.scavengers.find({ weapon: null })
